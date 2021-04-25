669295ebfcbbb1fdcfc9b1e7aef6423ac53e81b7
Arzuaman71 <arzuman.kochoyan98@mail.ru>
2021-04-25 22:24:29 +0400

    Bubble sort : 25/04/2021

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,9 +4,8 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] array = {1, 4, 5, 6, 8, 0, -1, -7};
-        int size = array.length - 1;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - i; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
 
                 if (array[j + 1] > array[j]) {
                     int temp = array[j];
